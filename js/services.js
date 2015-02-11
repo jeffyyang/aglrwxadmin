13d6c46044db41d1b8c1fb64f86e4a8fbaf03a89
jeffyyoung@gmail.com <jeffyyoung@gmail.com>
2015-02-11 17:49:37 +0800

    angular webi

--- js/services.js
+++ js/services.js
@@ -2,6 +2,39 @@
 
 /* Services */
 
-
 // Demonstrate how to register services
 angular.module('app.services', []);
+
+appServices.factory('accessService', function($rootScope) {
+    var accessService = {};
+ 
+
+    accessService.login = function(type, msg) {
+
+    };
+ 
+    accessService.logout = function(alert) {
+
+    };
+ 
+    return accessService;
+});
+
+appServices.factory('userService', function($rootScope) {
+    var userService = {};
+ 
+    userService.add = function(user) {
+
+    };
+ 
+    userService.del = function(userId) {
+
+    };
+ 
+    userService.update = function(user) {
+
+
+    };
+ 
+    return userService;
+});
